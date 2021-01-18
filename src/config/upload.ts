ac088b67542725fc56b1fa809b8478b1e8934a0b
Raphael Sampaio <sampaiodeveloper@gmail.com>
2021-01-18 17:08:19 -0300

    :sparkles: challenge finalized

---
+++ src/config/upload.ts
@@ -0,0 +1,19 @@
+import * as path from 'path';
+import * as crypto from 'crypto';
+import * as multer from 'multer';
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
+export default {
+  directory: tmpFolder,
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
